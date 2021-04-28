aeec9bdc74a82844deef8ae0ebdb95d0b7828b45
Jayvee Novilla <jnovilla6395@sdsu.edu>
2021-04-28 14:45:24 -0700

    added darkened image to game over menu. repositioned and resized buttons to match main menu.

--- Assets/Scripts/GameOverMenu.cs
+++ Assets/Scripts/GameOverMenu.cs
@@ -7,25 +7,28 @@ using UnityEngine.SceneManagement;
 public class GameOverMenu : MonoBehaviour
 {
     public MenuController menuController;
+    public GameOverMenu gameOverMenu;
 
     public Button restartButton;
 
-    public Canvas gameOverMenu;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        gameOverMenu = GameObject.Find("Game Over Menu").GetComponent<Canvas>();    //script attached to GameOverMenu canvas in play scene
+        gameOverMenu = GameObject.FindObjectOfType<GameOverMenu>();
         menuController = GameObject.FindObjectOfType<MenuController>();
         if (menuController.CheckButton("Restart Button"))       //menu controller already has main menu and quit buttons. added restart button for play scene
         {
             restartButton = GameObject.Find("Restart Button").GetComponent<Button>();
             restartButton.onClick.AddListener(() => menuController.LoadScene(2));
         }
-        gameOverMenu.gameObject.SetActive(false);      //inactivates game over menu at start of play scene. comment this line out to test button functionality
+        gameOverMenu.gameObject.SetActive(false);      //inactivates game over menu at start of playtest scene
     }
 
     public void GameOver()   //game over menu to be used when player runs out of lives
     {
         gameOverMenu.gameObject.SetActive(true);    //deactivates game over menu
     }
+
 }
