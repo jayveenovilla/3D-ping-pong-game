6db905dcdaee5fdb669219e1610018494a908b3d
Viibezz <brand@live.com>
2021-04-28 16:05:36 -0700

    implemented bonus powerup spawn and destroy on trigger

---
+++ Assets/Scripts/DestroyBonus.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyBonus : MonoBehaviour
+{
+    public SpawnBonus spawnBonus;
+
+    public void Start()
+    {
+        spawnBonus = GameObject.Find("BonusSpawn").GetComponent<SpawnBonus>();
+    }
+    private void OnTriggerEnter(Collider c)
+    {
+        if (c.gameObject.tag == "Ball")
+        {
+            //plus points todo
+            spawnBonus.onTableCount--;
+            Destroy(gameObject);
+        }
+    }
+}
