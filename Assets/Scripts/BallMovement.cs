18f040db47fd30dedd262eeb24dae4426bde6c3e
Jayvee Novilla <jnovilla6395@sdsu.edu>
2021-05-01 13:36:09 -0700

    updating to new master. reverted changes to be applied after update.

--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class BallMovement : MonoBehaviour {
     [SerializeField]
     private float ballSpeed;
-    private bool isBallMoving;
 
     private Vector3 ballDirection;
     private Vector3 ballStartPosition;
@@ -25,30 +24,24 @@ public class BallMovement : MonoBehaviour {
         myPlayerLives = Paddle.GetComponent<PlayerLives>();
         rb = GetComponent<Rigidbody>();
         ballStartPosition = transform.position;         //start position of ball
-        isBallMoving = false;       //used for BallMove function to prevent spam of ball movement
+        MoveBall();
     }
 
     // Update is called once per frame
     void FixedUpdate() {
         rb.MovePosition(transform.position + ballDirection * Time.deltaTime * ballSpeed);
-    }
 
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            if (!isBallMoving)      //launches ball only when ball is stationary
-            {
-                MoveBall();
-            }
+        //resets position of ball to center
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            MoveBall();
         }
+
     }
 
     private void MoveBall() {
-        isBallMoving = true;
-        rb.MovePosition(ballStartPosition);
-        float x = Random.Range(0, 2) == 0 ? -1 : 1;       //set to -1 to launch toward player paddle
-        float z = Random.Range(0, 2) == 0 ? -1 : 1;       //set to -1 to launch toward player paddle
+        transform.position = ballStartPosition;
+        float x = Random.Range(0, 2) == 0 ? -1 : 1;
+        float z = Random.Range(0, 2) == 0 ? -1 : 1;
         ballDirection = new Vector3(x, 0, z).normalized;        //locks movement of ball to x and z axis only. it is based on position of our playing field
     }
 
@@ -61,26 +54,20 @@ public class BallMovement : MonoBehaviour {
             myPlayerLives.playerDecreaseLives();          //call gameover function in gameovermenu script
             if (GameManager._instance.player.playerLives > 0)
             {
-                isBallMoving = false;
-                ballDirection = new Vector3(0, 0, 0).normalized;    //resets vector to 0s for restart position so ball won't launch until MoveBall function
-                transform.position = ballStartPosition;     //if player still has lives then move the ball back to start position
+                MoveBall();     //if player still has lives then move the ball back to start position
             }
         }
+        else if (str == "PlayerPaddle" || str == "PlayerPaddle (1)")
+            ScoreManager.instance.AddScore();  //on paddle Collision add a point
 
-        if (str == "PlayerPaddle" || str == "PlayerPaddle (1)")
+        if (str == "YellowStar")
         {
-            Vector3 paddlePosition = other.transform.position;
-            Vector3 ballPosition = gameObject.transform.position;
-            Vector3 delta = ballPosition - paddlePosition;      //angle ball deflects off of paddle changes depending on position paddle is hit
-            Vector3 direction = delta.normalized;
-            ballDirection = direction;
+            Debug.Log("bonus tag works");
         }
-        else
-        {
-            ContactPoint contact = other.GetContact(0);
-            Vector3 normal = contact.normal;
-            ballDirection = Vector3.Reflect(ballDirection, normal);     // Makes the reflected object appear opposite of the original object     
-        }
+
+        ContactPoint contact = other.GetContact(0);
+        Vector3 normal = contact.normal;
+        ballDirection = Vector3.Reflect(ballDirection, normal);     // Makes the reflected object appear opposite of the original object     
     }
 
     private void OnTriggerEnter(Collider c)
