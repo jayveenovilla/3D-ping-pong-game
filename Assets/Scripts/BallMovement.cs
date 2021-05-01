171213ade6f0fe65d1a9f2340d3a50528a10b4ef
Jayvee Novilla <jnovilla6395@sdsu.edu>
2021-04-30 23:53:27 -0700

    added particle effects collission script for blue smoke and firework small. added 'bonus' tag to stars/coins.

--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -8,16 +8,19 @@ public class BallMovement : MonoBehaviour {
 
     private Vector3 ballDirection;
     private Vector3 ballStartPosition;
+    public Vector3 ballPosition;
     private Rigidbody rb;
 
     public GameObject Paddle;
     GameOverMenu myGameOverMenu;
     PlayerLives myPlayerLives;
+    ParticleEffects myParticleEffects;
 
     // Start is called before the first frame update
     void Start() {
         myGameOverMenu = GameObject.FindGameObjectWithTag("GameOver").GetComponent<GameOverMenu>();
         Paddle = GameObject.Find("PlayerPaddle");   //attached PlayerLives script to PlayerPaddle
+        myParticleEffects = GameObject.Find("ParticleEffects").GetComponent<ParticleEffects>(); ;  //attached ParticleEffects script to particleeffect object
         myPlayerLives = Paddle.GetComponent<PlayerLives>();
         rb = GetComponent<Rigidbody>();
         ballStartPosition = transform.position;         //start position of ball
@@ -46,18 +49,32 @@ public class BallMovement : MonoBehaviour {
         string str = other.gameObject.name;
         if (str == "Boundary Players Goal" || str == "Boundary Enemy Goal")     //check the boundary names
         {
+            ballPosition = rb.gameObject.transform.position;
+            StartCoroutine(myParticleEffects.blueSmoke());
             myPlayerLives.playerDecreaseLives();          //call gameover function in gameovermenu script
             if (GameManager._instance.player.playerLives > 0)
             {
                 MoveBall();     //if player still has lives then move the ball back to start position
             }
         }
-        else if (str == "PlayerPaddle")
-            ScoreManager.instance.AddScore();  //on paddle Collision add a point
+        
+        if (str == "YellowStar")
+        {
+            Debug.Log("bonus tag works");
+        }
 
         ContactPoint contact = other.GetContact(0);
         Vector3 normal = contact.normal;
         ballDirection = Vector3.Reflect(ballDirection, normal);     // Makes the reflected object appear opposite of the original object     
+    }
+
+    private void OnTriggerEnter(Collider c)
+    {
+        if (c.gameObject.tag == "Bonus")        //for use when the ball interacts with any of the stars/coins
+        {
+            ballPosition = c.gameObject.transform.position;
+            StartCoroutine(myParticleEffects.fireworkSmall());
+        }
     }
 }
 
